5d8a104c385fb80e7ded7bb7e0e51155219185ef
kkotto <ksen.sharupich.work@gmail.com>
2022-11-30 00:43:43 +0300

    Task one completed
    Took 24 minutes

--- src/main/java/com/kkotto/Main.java
+++ src/main/java/com/kkotto/Main.java
@@ -1,7 +1,17 @@
 package com.kkotto;
 
+import com.kkotto.service.TaskService;
+import com.kkotto.service.impl.TaskOneServiceImpl;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<TaskService> listOfTasks = new ArrayList<>();
+        listOfTasks.add(TaskOneServiceImpl.getInstance());
+        for (TaskService task : listOfTasks) {
+            task.runTask();
+        }
     }
 }
