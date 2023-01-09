d7406670dd1c1d3006eb2efada59f16e2075b250
kkotto <ksen.sharupich.work@gmail.com>
2023-01-09 22:41:17 +0300

    Task two completed
    Took 11 minutes

--- src/main/java/com/kkotto/Main.java
+++ src/main/java/com/kkotto/Main.java
@@ -2,6 +2,7 @@ package com.kkotto;
 
 import com.kkotto.service.TaskService;
 import com.kkotto.service.impl.TaskOneServiceImpl;
+import com.kkotto.service.impl.TaskTwoServiceImpl;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,6 +11,7 @@ public class Main {
     public static void main(String[] args) {
         List<TaskService> listOfTasks = new ArrayList<>();
         listOfTasks.add(TaskOneServiceImpl.getInstance());
+        listOfTasks.add(TaskTwoServiceImpl.getInstance());
         for (TaskService task : listOfTasks) {
             task.runTask();
         }
